9d985a1736122083dbf7482745f3b06c452981a2
Bryan Choong <bryanchoong12@gmail.com>
2023-03-02 21:51:23 +0800

    gui

---
+++ src/main/java/Helpers/Roles.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Helpers;
+
+/**
+ *
+ * @author Bryan
+ */
+public enum Roles {
+    Resident,
+    Vendor,
+    Cleaner,
+    Admin,
+    BuildingExecutive,
+    AccountExecutive,
+}
